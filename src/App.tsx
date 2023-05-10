85e81448549451bb5f1b019cbafae895c4c54373
kris <kmsedu@tuta.io>
2023-05-11 08:47:23 +1000

    Added mock About component/section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./layout/Header.tsx";
+import { About } from "./layout/About.tsx";
 
 export function App() {
   return (
     <>
       <Header />
+      <About />
     </>
   );
 }
---
+++ src/layout/About.tsx
@@ -0,0 +1,13 @@
+export function About() {
+  return (
+    <section className="bg-[#D9D9D9] px-6 py-6 text-[#272727] -z-10 relative">
+      <h2 className="font-title text-2xl">About</h2>
+      <p className="font-body leading-7 max-w-lg mt-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+    </section>
+  );
+}
