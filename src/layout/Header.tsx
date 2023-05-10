8e313f6ec1dbce157dfb489be4bea5abe055083f
kris <kmsedu@tuta.io>
2023-05-11 08:46:58 +1000

    Added relative position to Header for z-index and reduced flex gap

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,11 +1,11 @@
 export function Header() {
   return (
-    <header className="sm:flex sm:justify-between sm:items-center sm:pr-8 shadow-md shadow-gray-300">
+    <header className="sm:flex sm:justify-between sm:items-center sm:pr-8 shadow-sm shadow-gray-400 z-10 relative">
       <h1 className="flex px-3 py-1 text-[40px] text-[#024D78] font-title leading-none pt-4 cursor-pointer">
         KS
       </h1>
       <nav>
-        <ul className="bg-[#272727] flex justify-between text-white font-title sm:bg-white sm:text-[#024d78] sm:gap-8">
+        <ul className="bg-[#272727] flex justify-between text-white font-title sm:bg-white sm:text-[#024d78] sm:gap-4">
           <a
             href="#"
             className="nav-button hover:bg-[#024D78] active:bg-[#0879B9] transition-colors sm:hover:bg-[#fff] sm:active:bg-[#fff] min-w-[68px]"
