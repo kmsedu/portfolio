20f55955e5f509a0fa6c1a3f347f8750705032d1
kris <kmsedu@tuta.io>
2023-05-13 06:50:34 +1000

    Styling for Card component, decatted projects.json & removed vite icon from /public/

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,7 @@
 import { CardProps } from "../types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export function Card({
   imageUrl,
@@ -8,20 +11,38 @@ export function Card({
   liveUrl,
 }: CardProps) {
   return (
-    <figure className="flex flex-col border border-red-500 bg-white">
-      <h3>{name}</h3>
+    <figure className="flex flex-col bg-white text-[#272727] hover:scale-[101%] transition-transform duration-500">
+      <h3 className="font-title text-xl p-2 pt-4 bg-[#024d78] text-white shadow-sm shadow-gray-700 relative z-10">
+        {name}
+      </h3>
       <img
         src={imageUrl}
         alt={name}
-        className="object-cover object-top relative max-h-[200px] flex"
+        className="object-cover object-left-top relative max-h-[200px] flex border-b border-gray-300"
       />
-      <figcaption>
-        <p>{description}</p>
-      </figcaption>
-      <menu className="flex justify-evenly">
-        <p>{sourceUrl}</p>
-        <p>{liveUrl}</p>
-      </menu>
+      <div className="flex flex-col justify-between flex-1">
+        <figcaption className="flex-1">
+          <p className="font-body p-6 leading-8 text-s text-gray-600">
+            {description}
+          </p>
+        </figcaption>
+        <menu className="flex justify-evenly flex-1 items-end mb-4 text-3xl text-gray-600">
+          <a
+            href={liveUrl}
+            target="_blank"
+            className="hover:text-gray-800 transition-colors duration-500"
+          >
+            <FontAwesomeIcon icon={faGlobe} />
+          </a>
+          <a
+            href={sourceUrl}
+            target="_blank"
+            className="hover:text-gray-800 transition-colors duration-500"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </menu>
+      </div>
     </figure>
   );
 }
